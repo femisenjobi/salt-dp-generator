29b1b17af7ecfd52e51301e1ddf730222ecd22f2
Femi Senjobi <femi.senjobi@andela.com>
2020-08-19 11:53:40 +0100

    Generate DP for salt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './logo.jpeg';
+import placeHolder from './sample.jpeg';
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState();
+  const [loading, setloading] = useState(false);
+  const uploadWidget = () => {
+    setloading(true);
+    window.cloudinary.openUploadWidget(
+      { cloud_name: 'dmlyic7tt', upload_preset: 'ml_default'},
+      function(error, result) {
+        console.log(result);
+        setImage(`https://res.cloudinary.com/dmlyic7tt/image/upload/w_1080,h_1080,c_fill/l_${result[0].public_id},w_510,h_510,c_fill,x_-250,y_225,r_max/salt-round.jpg`);
+        setloading(false)
+      },
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 col-lg-6 offset-md-2 offset-lg-3">
+          <div className="row d-flex justify-content-center">
+            <img src={logo} alt="logo" className="img-responsive" style={{height: '120px'}} />
+          </div>
+          <div className="row d-flex justify-content-center">
+            <h3>Create your own custom DP</h3>
+          </div>
+          <div className="row d-flex justify-content-center mb-3">
+            {
+              loading ?
+              <div class="spinner-border" role="status" style={{height: '200px', width: '200px', margin: '100px'}}>
+                <span class="sr-only">Loading...</span>
+              </div> :
+              <img src={image || placeHolder} alt="logo" className="img-responsive" style={{height: '400px'}} />
+            }
+          </div>
+          {image && !loading &&
+            <div className="row d-flex justify-content-center mb-3">
+              <a href={image} download className="btn btn-primary">
+                Download Image
+              </a>
+            </div>
+          }
+          <div className="row d-flex justify-content-center">
+            <div className="main">
+              <div className="upload text-center">
+                <button onClick={uploadWidget} className="upload-button btn btn-primary">
+                  Upload Image
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
