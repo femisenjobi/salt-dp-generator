099fd05dda7d75a5e1b79c3bf79d389a57bf78b3
Femi Senjobi <femijohn25@gmail.com>
2025-06-06 10:18:05 -0400

    Allow custom logo creation

--- src/DpGenerator.js
+++ src/DpGenerator.js
@@ -32,42 +32,16 @@ function DpGenerator({ width, height, xPos, yPos, mainImage, radius, logoImage, 
       // For non-preview (interactive mode)
       if (uploadedUserPhotoId) {
         // If user has uploaded their photo, mainImage is the event's base image, logoImage is the event's overlay
-        finalUrl = `https://res.cloudinary.com/dmlyic7tt/image/upload/w_1080,h_1080,c_fill/l_${logoImage},w_${width},h_${height},c_fill,x_${xPos},y_${yPos},r_${radius}/l_${uploadedUserPhotoId},c_fill,w_1080,h_1080/${mainImage}`;
-        // The above line seems overly complex. The standard is usually:
-        // Base image is `mainImage`. User uploads `uploadedUserPhotoId`. Logo is `logoImage`.
-        // The structure from original `uploadWidget` was:
-        // `https://.../l_${result.info.public_id},w_${width},h_${height}...,r_${radius}/${mainImage}`
-        // Here, result.info.public_id was the user's photo, and mainImage was the base template image.
-        // And logoImage was the one at the top, not part of this transformation.
-        // Let's stick to the original transformation logic for overlaying on a base image.
-        // The `logoImage` prop is the one applied as an overlay. `mainImage` is the base.
-        // `uploadedUserPhotoId` is the photo that gets composed ONTO the `mainImage`.
-        // This interpretation might be wrong. Let's assume the original structure:
-        // User uploads *their* photo. This photo is then overlaid with `logoImage`.
-        // The `mainImage` prop is the *base background* for the event.
-        // So, `l_USER_PHOTO_ID,.../l_LOGO_ID,.../BASE_EVENT_IMAGE`
-        // Or, more likely: `l_LOGO_ID,w_LOGO_WIDTH,... / USER_UPLOADED_PHOTO_ID` (if mainImage is not used when user uploads own)
-        // Or: `l_LOGO_ID,w_LOGO_WIDTH,... / BASE_IMAGE_FOR_EVENT` (if user only customizes placement of logo on predefined event base)
-
-        // Re-evaluating: In DpGenerator, `mainImage` is the big placeholder. `logoImage` is the small one at top.
-        // User uploads *their* picture, and this picture gets the `logoImage` (from props) overlaid.
-        // So, the `mainImage` prop is actually the one being *replaced* by user's upload.
-        // And `logoImage` is the overlay.
-        // `setImage(`https://.../l_${result.info.public_id},w_${width},h_${height},c_fill,x_${xPos},y_${yPos},r_${radius}/${mainImage}`);`
-        // This means: `l_LOGO_PROP_ID, ... / USER_UPLOADED_PHOTO_ID` --> This is wrong.
-        // It should be: `l_LOGO_PROP_ID, ... / MAIN_IMAGE_PROP` (if user doesn't upload)
-        // OR `l_LOGO_PROP_ID, ... / USER_UPLOADED_PHOTO_ID` (if user uploads)
-
-        // Let's simplify: `generatedImageUrl` is the image shown in the large preview spot.
-        // If user uploads their photo (`uploadedUserPhotoId`), that becomes the base for the overlay.
-        // If not, `mainImage` (prop) is the base.
-        // `logoImage` (prop) is always the overlay.
+        finalUrl = `https://res.cloudinary.com/dmlyic7tt/image/upload/w_1080,h_1080,c_fill/l_${uploadedUserPhotoId},w_${width},h_${height},c_fill,x_${xPos},y_${yPos},r_${radius}/${mainImage}`;
+        // If user has uploaded their photo, we use it as the overlay
         const baseForOverlay = uploadedUserPhotoId || mainImage;
-        finalUrl = `https://res.cloudinary.com/dmlyic7tt/image/upload/w_1080,h_1080,c_fill/l_${logoImage},w_${width},h_${height},c_fill,x_${xPos},y_${yPos},r_${radius}/${baseForOverlay}`;
+        finalUrl = `https://res.cloudinary.com/dmlyic7tt/image/upload/w_1080,h_1080,c_fill/l_${baseForOverlay},w_${width},h_${height},c_fill,x_${xPos},y_${yPos},r_${radius}/${mainImage}`;
 
         // Update download link
         if (uploadedUserPhotoId) { // Only allow download if user has uploaded their base image
-             setDownloadLink(`https://res.cloudinary.com/dmlyic7tt/image/upload/fl_attachment:my_dp,w_1080,h_1080,c_fill/l_${logoImage},w_${width},h_${height},c_fill,x_${xPos},y_${yPos},r_${radius}/${uploadedUserPhotoId}`);
+             setDownloadLink(
+               `https://res.cloudinary.com/dmlyic7tt/image/upload/fl_attachment:my_dp,w_1080,h_1080,c_fill/l_${uploadedUserPhotoId},w_${width},h_${height},c_fill,x_${xPos},y_${yPos},r_${radius}/${mainImage}`
+             );
         } else {
             setDownloadLink(''); // No download if it's just the placeholder/template base
         }
@@ -158,9 +132,6 @@ function DpGenerator({ width, height, xPos, yPos, mainImage, radius, logoImage, 
               </div>
             </>
           )}
-        </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
